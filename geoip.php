235ac3096e7915dba34313ff9a80f3d4ac0436f8
asiekierka <asiekierka@gmail.com>
2013-03-23 12:12:14 +0100

    more information

--- geoip.php
+++ geoip.php
@@ -12,25 +12,26 @@
   // 2 - Ask To Allow (button mandatory, opt-in/opt-out type determined by other variable)
   // 1 - Information, no button mandatory
   // 0 - No need to confirm
-  // + opt+out - user must opt-out and not opt-in
+  // + opt_out - user must opt-out and not opt-in
   // + session_allowed - non-intrusive cookies allowed
   // + detailed - information about cookies has to be detailed
   // Unsure about: Belgium, Denmark (opt-out with caution!), Germany (unclear, but german sites DGAF), Italy?, Malta
   // , Portugal
-  $country_2 = array("AT","CY","FI","FR","GR","IT","LT","LV","LU","NL","PT","RO","SK","ES","SE");
-  $country_1 = array("DK","PL","BE","BG","HU","IE","PL");
-  $country_0 = array("CZ","EE","DE","MT","SI");
-  $country_di = array("BE","BG","FR","PL");
-  $country_oo = array("CZ","FI","IT");
-  $country_sa = array("BE","DE");
+  $country_2 = array("AT","CY","FR","GR","IT","LT","LV","LU","NL","PT","ES","SE");
+  $country_1 = array("CZ","DK","EE","FI","PL","BE","BG","HU","IE","PL","SK","MT","RO");
+  $country_0 = array("DE","MT","SI");
+  $country_di = array("BE","BG","FR","HU","PL","RO","SK");
+  $country_oo = array("CZ","EE","FI","IT");
+  $country_sa = array("BE","DE","IT","NL","PL");
   $country_val = 2;
   if(!in_array($country,$eu_countries)) $country_val = 0;
   else {
     if(in_array($country,$country_1)) $country_val = 1;
     if(in_array($country,$country_0)) $country_val = 0;
   }
-  print("{ mode: ".$country_val.", ");
+  print("{ country: '".$country."', mode: ".$country_val.", ");
   print("detailed_information: ".(in_array($country,$country_di)?"true":"false").", ");
   print("opt_out: ".(in_array($country,$country_oo)?"true":"false").", ");
+  print("browser_settings: ".(in_array($country,$country_bs)?"true":"false").", ");
   print("session_allowed: ".(in_array($country,$country_sa)?"true":"false")." }");
 ?>
